dc4f82947a88066c0df558e96007dccead594ad7
jCasia <casjay70@gmail.com>
2023-05-17 23:37:27 +0100

    added some more media queries

--- src/pages/DesignedFor.jsx
+++ src/pages/DesignedFor.jsx
@@ -1,17 +1,31 @@
 import styles from './styles/DesignedFor.module.scss';
-import image from '../assets/images/illustration-editor-mobile.svg';
+import imageMobile from '../assets/images/illustration-editor-mobile.svg';
+// import imageDesktop from '../assets/images/illustration-editor-desktop.svg';
 import ContentPara from '../components/ContentPara';
 import { dataDesignFor } from '../../data';
 
 const DesignedFor = () => {
   return (
     <section className={styles.designedFor}>
+      {/* <img
+        src={imageDesktop}
+        alt='editor illustration'
+        role='presentation'
+        className={styles.imageDesktop}
+      /> */}
       <h2>Designed for the future</h2>
-      <img src={image} alt='' role='presentation' />
-      {dataDesignFor.map((data) => {
-        const { heading, text, id } = data;
-        return <ContentPara key={id} heading={heading} text={text} />;
-      })}
+      <img
+        src={imageMobile}
+        alt='editor illuestartion'
+        role='presentation'
+        className={styles.imageMobile}
+      />
+      <div className={styles.content}>
+        {dataDesignFor.map((data) => {
+          const { heading, text, id } = data;
+          return <ContentPara key={id} heading={heading} text={text} />;
+        })}
+      </div>
     </section>
   );
 };
--- src/pages/FreeOpen.jsx
+++ src/pages/FreeOpen.jsx
@@ -1,16 +1,30 @@
 import styles from './styles/FreeOpen.module.scss';
-import laptopImage from '../assets/images/illustration-laptop-mobile.svg';
+import laptopImageMobile from '../assets/images/illustration-laptop-mobile.svg';
+import laptopImageDesktop from '../assets/images/illustration-laptop-desktop.svg';
 import { dataFreeOpen } from '../../data';
 import ContentPara from '../components/ContentPara';
 
 const FreeOpen = () => {
   return (
     <section className={styles.freeOpen}>
-      <img src={laptopImage} alt='illustration laptop' role='presentation' />
-      {dataFreeOpen.map((data) => {
-        const { id, heading, text } = data;
-        return <ContentPara key={id} heading={heading} text={text} />;
-      })}
+      <img
+        src={laptopImageDesktop}
+        alt='illustration laptop'
+        role='presentation'
+        className={styles.imageDesktop}
+      />
+      <img
+        src={laptopImageMobile}
+        alt='illustration laptop'
+        role='presentation'
+        className={styles.imageMobile}
+      />
+      <div className={styles.content}>
+        {dataFreeOpen.map((data) => {
+          const { id, heading, text } = data;
+          return <ContentPara key={id} heading={heading} text={text} />;
+        })}
+      </div>
     </section>
   );
 };
